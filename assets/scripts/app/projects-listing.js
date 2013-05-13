70fbae39184a4b16e9d402abd58e3ed30d79d6bb
Alessandro Molari <molari.alessandro@gmail.com>
2013-05-13 23:40:52 +0200

    added buttons; syntax is now compressed :)

--- assets/scripts/app/projects-listing.js
+++ assets/scripts/app/projects-listing.js
@@ -1,80 +1,103 @@
 define([
   'jquery', 'ejs/ejs', 'sugar'
-], function($, ejs, sugar) {
+], function ($, ejs, sugar) {
 
-  function removeDisabledProjects(db, disabled_metadata) {
-    db.projects.remove(function(project) {
-      return disabled_metadata.find(function(disabled_project_name) {
-        return (project.name == disabled_project_name);
+  /**
+   * Remove the disabled projects ('disbl_prj_names') from the 'db'
+   *
+   * @param db
+   * @param {[String]} disbl_prj_names List of projects names to be disabled
+   */
+  function rm_disbl_prjs (db, disbl_prj_names) {
+    db.prjs.remove(function(prj) {
+      return disbl_prj_names.find(function(disbl_prj_name) {
+        return (prj.name == disbl_prj_name);
       }) !== undefined;
     });
   }
 
-  function setupProjectsContent(db, projects_metadata) {
-    var pm = projects_metadata.projects; /* alias */
+  /**
+   * Override the property 'src_prop' (in 'prj')
+   * with the 'trgt_prop' (if available in 'cust_mtd.prjs[prj.name]');
+   * otherwise with the 'fallback' value in the project metadata 'prj'
+   *
+   * @param prj
+   * @param cust_mtd
+   * @param src_prop
+   * @param trgt_prop
+   * @param fallback
+   */
+  function override_prop (prj, cust_mtd, src_prop, trgt_prop, fallback) {
+    if (cust_mtd.prjs && cust_mtd.prjs.hasOwnProperty(prj.name)) {
+      prj_cust_mtd = cust_mtd.prjs[prj.name];
+      if (prj_cust_mtd.hasOwnProperty(src_prop)) {
+        prj[trgt_prop] = prj_cust_mtd[src_prop];
+      } else if (fallback !== undefined) { prj[trgt_prop] = fallback; }
+    } else if (fallback !== undefined) { prj[trgt_prop] = fallback; }
+  }
 
-    db.projects.each(function(project, index) {
+  /**
+   * Setup the 'db' from the provided 'gh_mtd' and 'prjs_mtd'
+   *
+   * @param db Target database
+   * @param gh_mtd Github metadata
+   * @param cust_mtd Custom metadata
+   */
+  function setup_db (db, gh_mtd, cust_mtd) {
+    /* Initially setup the db 'prjs' property with the github metadata */
+    db.prjs = gh_mtd;
 
-      //                                                 1. Shown Name
-      if (projects_metadata.hasOwnProperty('projects') &&
-        pm.hasOwnProperty(project.name)) {
-        if (pm[project.name].hasOwnProperty('replaceNameWith')) {
-          project.shownName = pm[project.name].replaceNameWith;
-        } else { project.shownName = project.name; }
-      } else { project.shownName = project.name; }
-      project.shownName = project.shownName.titleize();
+    /* Remove the disabled projects */
+    if (cust_mtd.disbl) {
+      cust_mtd.disbl = Object.isArray(cust_mtd.disbl) ? cust_mtd.disbl : [cust_mtd.disbl];
+      rm_disbl_prjs(db, cust_mtd['disbl']);
+    }
 
-      //                                                 2. Description
-      if (project.hasOwnProperty('description')) {
-        project.hasDetails = true;
-      }
-      if (projects_metadata.hasOwnProperty('projects') &&
-        pm.hasOwnProperty(project.name)) {
-        if (pm[project.name].hasOwnProperty('replaceDescWith')) {
-          project.shownDesc = pm[project.name].replaceDescWith;
-        } else { project.shownDesc = project.description; }
-      } else { project.shownDesc = project.description; }
+    /* Setup each single project metadata */
+    db['prjs'].each(function(prj) {
 
-      //                                                 3. Buttons
-      project.buttons = [];
-      var push_button = function(name, link, in_title, in_content) {
-        project.buttons.push({ name: name, link: link,
-          in_title: in_title, in_content: in_content });
+      /* 1. Shown Name */
+      override_prop(prj, cust_mtd, 'repl_name_with', 'shown_name', prj['name']);
+      prj.shown_name = prj.shown_name.titleize();
+
+      /* 2. Description */
+      prj.desc = prj.description; delete prj.description; /* 'description' -> 'desc' */
+      override_prop(prj, cust_mtd, 'repl_desc_with', 'shown_desc', prj['desc']);
+
+      /* 3. Buttons */
+      prj['btns'] = [];
+      var push_btn = function(name, link) {
+        prj['btns'].push({ name: name, link: link });
       };
-      //                                                   3.1 Source Code
-      push_button('source_code', project.svn_url, false, true);
-      //                                                   3.2 Wiki
-      if (project.hasWiki) {
-        push_button('wiki', project.svn_url + "/wiki", true, true);
+
+      /* 3.1 Button - Source Code */
+      push_btn('source_code', prj['svn_url']);
+
+      /* 3.2 Button - Wiki */
+      if (prj['has_wiki']) {
+        push_btn('wiki', prj['svn_url'] + "/wiki");
       }
+
+      /* has_details: The current project has some details ? {Boolean} */
+      prj['has_details'] = prj.hasOwnProperty('desc');
+      prj['has_details'] = prj['has_details'] || (prj['btns'].length > 0);
     });
   }
 
   $(document).ready(function() {
-    var wrapper_id = 'projects-content';
-    var github_url =
+    var wrp_id = 'prjs-ctnt';
+    var gh_mtd_url =
         'https://api.github.com/users/{un}/repos?'.assign({ un: 'alem0lars' });
-    var projects_metadata_url = '/data/projects.json';
-    var template_url = '/projects-listing.html';
-    var db;         // JSON database object that feeds the template
+    var cust_mtd_url = '/data/projects.json';
+    var templ_url = '/projects-listing.html';
+    var db = {}; /* JSON database object that feeds the template */
 
-    $.getJSON(github_url, function(github_data) {
-
-      db = { projects: github_data };
-
-      $.getJSON(projects_metadata_url, function(projects_metadata) {
-
-        if (projects_metadata.hasOwnProperty('disabled') &&
-            Object.isArray(projects_metadata.disabled)) {
-          removeDisabledProjects(db, projects_metadata.disabled);
-        }
-
-        setupProjectsContent(db, projects_metadata);
-
-        console.log(db);
-
-        new EJS({ url: template_url }).update(wrapper_id, db);
-
+    $.getJSON(gh_mtd_url, function(gh_mtd) {
+      $.getJSON(cust_mtd_url, function(cust_mtd) {
+        /* Setup the db using the github and custom metadata */
+        setup_db(db, gh_mtd, cust_mtd);
+        /* Parse template from 'template_url' into 'wrapper_id' using 'db' */
+        new EJS({ url: templ_url }).update(wrp_id, db);
       });
     });
   });
